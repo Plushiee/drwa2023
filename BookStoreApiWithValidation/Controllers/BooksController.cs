410e1db183826e123055949b535c71d909a2ed02
Plushiee <barasatyaradi@gmail.com>
2023-04-19 13:00:56 +0700

    tugas minggu 8 validasi model api

---
+++ BookStoreApiWithValidation/Controllers/BooksController.cs
@@ -0,0 +1,90 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+using BookStoreApi.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http.Filters;
+using System.Web.Http.Controllers;
+using System.Net;
+
+namespace BookStoreApi.Controllers;
+
+[ApiController]
+[Route("api/Books")]
+
+public class BooksController : ControllerBase
+{
+    private readonly BooksService _booksService;
+
+    public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+
+    [HttpPost]
+    [ValidateModel]
+    public async Task<IActionResult> Post(Book newBook)
+    { 
+        await _booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+
+        await _booksService.UpdateAsync(id, updatedBook);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await _booksService.RemoveAsync(id);
+
+        return NoContent();
+    }
+
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext.ModelState.IsValid == false)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
---
+++ BookStoreApiWithValidation/Models/Book.cs
@@ -0,0 +1,30 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")]
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string BookName { get; set; } = null!;
+    
+    [Required]
+    [Range(0, 999)]
+    public decimal Price { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string Category { get; set; } = null!;
+
+    [Required]
+    public string Author { get; set; } = null!;
+}
